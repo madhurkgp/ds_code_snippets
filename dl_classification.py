08b2483e4eeef1f2478209584fb5ddfbb8ba9307
madhur yadav <madhur.yadav7@gmail.com>
2020-06-08 15:24:25 +0530

    addming some links

---
+++ dl_classification.py
@@ -0,0 +1,4 @@
+repatedstratifiedkfold , dummyclassifier , load dataset, evaluate model , unbalanced multiclass classification, get_models, smote - https://machinelearningmastery.com/imbalanced-multiclass-classification-with-the-e-coli-dataset/
+
+prauc, precesion-recall curve - https://machinelearningmastery.com/imbalanced-classification-with-the-fraudulent-credit-card-transactions-dataset/
+
---
+++ ml_regression.py
@@ -0,0 +1,8 @@
+GBM
+Xgboost
+RandomForestClassifier
+Gradient Boosting classifier
+Extra Tree classifier
+Voting Classifier
+along with cross validation and gridsearch cv
+https://www.kaggle.com/ganeshmundra/prediction-using-6-different-model?scriptVersionId=28408887
